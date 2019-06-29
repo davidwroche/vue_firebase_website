a7057ec1b301eb66acdfd99bf7994937d72d65c5
davidwroche <davidwroche@yahoo.co.uk>
2019-06-29 21:31:05 +0100

    remove config keys from visible files

--- src/main.js
+++ src/main.js
@@ -2,21 +2,11 @@ import Vue from 'vue';
 import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
+import config from "../config"
 
 Vue.config.productionTip = false;
 
 let app = '';
-
-const config = {
-  apiKey: "AIzaSyALmEZm282yVzAj5sDrfDSYqzBngzYi7f4",
-  authDomain: "haggle-ed012.firebaseapp.com",
-  databaseURL: "https://haggle-ed012.firebaseio.com",
-  projectId: "haggle-ed012",
-  storageBucket: "haggle-ed012.appspot.com",
-  messagingSenderId: "134305249546",
-  appId: "1:134305249546:web:b6c179f2452c0c19"
-};
-
 
 firebase.initializeApp(config);
 
